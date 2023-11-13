273a46fe030de1a3a9613176ada2ea33fde49260
Maksym Habidulin <habidulin.m@gmail.com>
2023-11-13 17:05:28 +0100

    Stupid Coaching

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  # def about
+  #   @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+  # end
+
+  # def ask
+  # end
+
+end
